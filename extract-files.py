69edb76170a2bb8ec04f40a1ea8bb8df79fab299
Arian <arian.kulmer@web.de>
2024-08-29 16:59:23 +0200

    garnet: Patch keymint to use ndk backend
    Change-Id: I45d2e0adebdc3c1f63220698e283f0a6471bc350

--- extract-files.py
+++ extract-files.py
@@ -69,6 +69,18 @@ blob_fixups: blob_fixups_user_type = {
        'vendor/lib/libqtikeymint.so',
        'vendor/lib64/libqtikeymint.so'
     ): blob_fixup()
+        .replace_needed(
+            'android.hardware.security.keymint-V1-ndk_platform.so',
+            'android.hardware.security.keymint-V1-ndk.so',
+        )
+        .replace_needed(
+            'android.hardware.security.secureclock-V1-ndk_platform.so',
+            'android.hardware.security.secureclock-V1-ndk.so',
+        )
+        .replace_needed(
+            'android.hardware.security.sharedsecret-V1-ndk_platform.so',
+            'android.hardware.security.sharedsecret-V1-ndk.so',
+        )
         .add_needed('android.hardware.security.rkp-V1-ndk.so'),
     (
        'system_ext/lib/libwfdmmsrc_system.so',
