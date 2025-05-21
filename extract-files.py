f33ec3d2ffbae41b9336173d1172cfcc49ca6628
Michael Bestas <mkbestas@lineageos.org>
2025-05-21 12:19:01 +0530

    garnet: Patch libwfdservice to resolve duplicate dependencies
    Fixes:
    error: module "libwfdservice" variant
    "android_arm_armv8-a_cortex-a76_static": depends on multiple
    versions of the same aidl_interface:
    android.media.audio.common.types-V2-cpp-source,
    android.media.audio.common.types-V4-cpp-source
    
    Change-Id: I8086a80fcc238d6b03d16640ee2c4e134129bbab

--- extract-files.py
+++ extract-files.py
@@ -68,6 +68,11 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed(
+            'android.media.audio.common.types-V2-cpp.so',
+            'android.media.audio.common.types-V4-cpp.so',
+        ),
     (
        'vendor/bin/hw/android.hardware.security.keymint-service-qti',
        'vendor/lib/libqtikeymint.so',
