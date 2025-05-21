a2fc2bdd391acef2e732ea1b7271339c3135f158
AdarshGrewal <adarshgrewal@gmail.com>
2025-05-21 16:15:19 +0530

    lemonade: Enable elfchecks for camera libs 21
    Change-Id: Icdec7fc4b75bef1cb31463a7db028d8572b41a03

--- extract-files.py
+++ extract-files.py
@@ -111,10 +111,14 @@ blob_fixups: blob_fixups_user_type = {
             'android.hardware.gnss-V1-ndk.so'
     ),
     (
+        'vendor/lib64/libalhLDC.so',
+        'vendor/lib64/libalLDC.so',
+        'vendor/lib64/libTrueSight.so',
         'vendor/lib64/libMiVideoFilter.so'
     ): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
         .clear_symbol_version('AHardwareBuffer_lockPlanes')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
